9ee0a09af644219e94242b2fe4be3e87a7bbb871
spermspase <4itonada@gmail.com>
2018-01-23 11:05:51 +0400

    wtforms

---
+++ app/routes.py
@@ -0,0 +1,32 @@
+from app import app
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+	user = {'username': 'Pavel'}
+	posts = [
+		{
+			'autor': {'username': 'Loh'},
+			'body': 'Ebat` kolotit`',
+		},
+		{
+			'autor': {'username': 'Petya'},
+			'body': 'Nu nihuya sebe'
+		},
+		{
+			'autor': {'username': 'Vasya'},
+			'body': 'Ebushki Vorobushki'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Вход для пользователя {}, remember_me={}'.format(
+			form.username.data, form.remember_me.data))
+		return redirect('/index')
+	return render_template('login.html', title='Sing In', form=form)
