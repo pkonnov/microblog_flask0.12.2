54a0d81a8c2a97ebad88757a6b8fa9a04cfe669f
spermspase <4itonada@gmail.com>
2018-01-23 11:09:56 +0400

    start url_for

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/')
@@ -28,5 +28,5 @@ def login():
 	if form.validate_on_submit():
 		flash('Вход для пользователя {}, remember_me={}'.format(
 			form.username.data, form.remember_me.data))
-		return redirect('/index')
+		return redirect(url_for('index'))
 	return render_template('login.html', title='Sing In', form=form)
