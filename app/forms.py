56d0fc2b57f2395a98c34e8e9eced77e57659ce8
spermspase <4itonada@gmail.com>
2018-01-25 11:52:49 +0400

    registration

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, DataRequired, Email, EqualoTo
+from app.models import User
 
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired(), Email()])
 	password = PasswordField('Passowrd', validators=[DataRequired()])
+	password2 = PasswordField('Repeat Password',
+		validators=[DateRequired(), EqualTo('password')])
 	remember_me = BooleanField('Remember me')
-	submit = SubmitField('Sign In')
+	submit = SubmitField('Register')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user is not None:
+			raise ValidationError('Введите коректное имя.')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is not None:
+			raise ValidationError('Пожалуйста введите коректный адрес почты')
