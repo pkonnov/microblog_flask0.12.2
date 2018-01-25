5c0245cd7687ed1cf4da221eaa599ffb8a3c6b8b
spermspase <4itonada@gmail.com>
2018-01-25 09:52:15 +0400

    hash pass, UserMixin

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -24,9 +26,20 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
 	form = LoginForm()
 	if form.validate_on_submit():
-		flash('Вход для пользователя {}, remember_me={}'.format(
-			form.username.data, form.remember_me.data))
+		user = User.query.filter_by(username=form.username.data).first()
+		if user is none or not user.check_password(form.password.data):
+			flash('Не валидные данные')
+			return redirect(url_for('login'))
+		login_user(user, remember=form.remember_me.data)
 		return redirect(url_for('index'))
 	return render_template('login.html', title='Sing In', form=form)
+
+
+app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
