74318d474aa4c5c72a449dd5517423dc7e685ba8
spermspase <4itonada@gmail.com>
2018-01-25 10:56:51 +0400

    View function did not return a response

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
+from flask_login import current_user, login_user, logout_user, login_required
 from app.forms import LoginForm
 from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
 	user = {'username': 'Pavel'}
 	posts = [
@@ -31,15 +33,17 @@ def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		user = User.query.filter_by(username=form.username.data).first()
-		if user is none or not user.check_password(form.password.data):
+		if user is None or not user.check_password(form.password.data):
 			flash('Не валидные данные')
 			return redirect(url_for('login'))
 		login_user(user, remember=form.remember_me.data)
-		return redirect(url_for('index'))
-	return render_template('login.html', title='Sing In', form=form)
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page = url_for('index')
+		return redirect(next_page)
+	
 
-
-app.route('/logout')
+@app.route('/logout')
 def logout():
 	logout_user()
 	return redirect(url_for('index'))
