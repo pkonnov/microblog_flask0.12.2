fdf2eae6a51660d2e3bdd2bc6ba6bb4c0b8509b1
spermspase <4itonada@gmail.com>
2018-01-25 12:49:31 +0400

    blya ya loh

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,28 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
 
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
-	email = StringField('Email', validators=[DataRequired(), Email()])
 	password = PasswordField('Passowrd', validators=[DataRequired()])
-	password2 = PasswordField('Repeat Password',
-		validators=[DataRequired(), EqualTo('password')])
 	remember_me = BooleanField('Remember me')
-	submit = SubmitField('Register')
+	submit = SubmitField('Sign In')
 
-	def validate_username(self, username):
-		user = User.query.filter_by(username=username.data).first()
-		if user is not None:
-			raise ValidationError('Введите коректное имя.')
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
 
-	def validate_email(self, email):
-		user = User.query.filter_by(email=email.data).first()
-		if user is not None:
-			raise ValidationError('Пожалуйста введите коректный адрес почты')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
