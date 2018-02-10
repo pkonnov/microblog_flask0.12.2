a31f4faa83da95588cd9be07b6fc599a88f2f432
cretyne <4itonada@gmail.com>
2018-02-10 17:41:10 +0400

    smtp one method tests

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+import logging
+from logging.handlers import SMTPHandler
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -7,3 +9,21 @@ class Config(object):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = os.environ.get(int('MAIL_PORT' or 25))
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	ADMIN = ['admin@example.com']
+	if not app.debug:
+		if app.config['MAIL_SERVER']:
+			auth = None
+			if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+				auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PORT'])
+			secure = None
+			if app.config['MAIL_USE_TLS']:
+				secure = ()
+			mail_handler = SMTPHandler(
+				mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+
+				)
